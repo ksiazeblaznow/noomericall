62655303f35ab3ff69f9b8c08b75821afd92c291
ksiazeblaznow <ksiazeblaznow@gmail.com>
2021-05-11 17:49:44 +0200

    legendre i simpson dzialajacy prawdodepobosdabieniere

--- zad4/functions.py
+++ zad4/functions.py
@@ -4,13 +4,14 @@ import numpy as np
 # f     : wzór funkcji
 # a, b  : granice całkowania [a,b]
 # N     : ilość interwałów (liczba parzysta)
-def simpson(f,a,b,N=1000, epsilon=0.01):
+def simpson(f,a,b,epsilon=0.01):
 
+    nn=1000
     n = 2
     lastS = 0.0
     firstIter = True
     
-    while n < N:
+    while n < nn:
         dx = (b-a) / n
         x = np.linspace(a,b,n+1)
         y = f(x)
@@ -23,13 +24,11 @@ def simpson(f,a,b,N=1000, epsilon=0.01):
         lastS = S
         n += 2
         firstIter = False
-
-        print('lastS-S= ', lastS, S)
         
     return S, int(n/2)
 
 # wielomian Legendre'a
-def legendre(n, x):
+def legendre(a, b, n, x):
     if(n == 0):
         return 1 # P0 = 1
     elif(n == 1):
@@ -37,3 +36,26 @@ def legendre(n, x):
     else:
         return ( ((2 * n)-1) * x * legendre(n-1, x)-(n-1) * legendre(n-2, x)) / float(n)
 
+
+def legendre_gauss(n : int, p : float, k : float, fn):
+
+    Ak = [[1,1,0,0,0],
+    [0.55556, 0.88889, 0.55556,0,0],
+    [0.347855,0.652145,0.652145, 0.347855, 0],
+    [0.236927,0.478629,0.568889,0.478629,0.236927] ]
+    Xk = [
+    [-0.57735, 0.57735, 0, 0, 0],
+    [-0.774597, 0, 0.774597, 0, 0],
+    [-0.861136, -0.339981, 0.339981, 0.861136, 0],
+    [-0.906180, -0.538469, 0, 0.538469, 0.906180] ]
+
+    fac = 0.0
+    result = 0
+    factor = (k-p) / 2
+
+    for i in range(0, n):
+        fac = ((p+k) / 2) + (((k-p) / 2) * Xk[n-2][i])
+        result = result + Ak[n-2][i] * fn(fac)
+
+    result = factor * result
+    return result
--- zad4/zad4_po_raz_pierwszy.py
+++ zad4/zad4_po_raz_pierwszy.py
@@ -1,7 +1,7 @@
 import numpy as np
 import functions as fn
 import matplotlib.pyplot as plt
-from scipy import integrate
+
 
 # functions
 f1 = lambda x : x / np.sqrt(9 + x**4)
@@ -12,26 +12,8 @@ f5 = lambda x : 2*x - 7
 
 
 # simpson
-w = fn.simpson(f1, 0, 2, 10, 0.01)
+w = fn.simpson(f1, 0, 2, 0.01)
 print(w[0])
 
 # x = np.linspace(-1, 1, 10)
-# print( fn.legendre(4, f5(x)) )
-
-# print('scipy:', integrate.quad(f1, 0, 2)[0])
-
-
-
-
-
-# # legendre
-# m = fn.legendre(3, 5)
-# x = np.linspace(-1, 1, 200)
-
-# for i in range(1, 4):
-#     plt.plot(x, fn.legendre(i, x), label ="Legendre: " + str(i))
-
-# # plt.legend(loc ="best")
-# # plt.xlabel("X")
-# # plt.ylabel("Pn")
-# # plt.show()
+print( fn.legendre_gauss(5, -10, 10, f2))
