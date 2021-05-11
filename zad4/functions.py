6fa91a77853ed06173947d2f4d84eafe9883f349
ksiazeblaznow <ksiazeblaznow@gmail.com>
2021-05-11 05:46:23 +0200

    liczenie legendre'a (rekurencyjnie)

--- zad4/functions.py
+++ zad4/functions.py
@@ -1,9 +1,9 @@
 import numpy as np
 
+
 # f     : wzór funkcji
 # a, b  : granice całkowania [a,b]
 # N     : ilość interwałów (liczba parzysta)
-
 def simpson(f,a,b,N=50):
 
     if N % 2 == 1:
@@ -13,3 +13,12 @@ def simpson(f,a,b,N=50):
     y = f(x)
     S = dx/3 * np.sum(y[0:-1:2] + 4*y[1::2] + y[2::2])
     return S
+
+# wielomian Legendre'a
+def legendre(n, x): 
+    if(n == 0):
+        return 1 # P0 = 1
+    elif(n == 1):
+        return x # P1 = x
+    else:
+        return (((2 * n)-1) * x * legendre(n-1, x)-(n-1) * legendre(n-2, x)) / float(n)
--- zad4/zad4_po_raz_pierwszy.py
+++ zad4/zad4_po_raz_pierwszy.py
@@ -1,5 +1,6 @@
 import numpy as np
 import functions as fn
+import matplotlib.pyplot as plt
 
 # functions
 f1 = lambda x : x / np.sqrt(9 + x**4)
@@ -7,6 +8,17 @@ f2 = lambda x : 3*x**2
 
 
 # code
-w = fn.simpson(f1, 0, 2, 10)
+w = fn.simpson(f1, 0, 2, 8)
+print( w )
 
-print(w)
+
+m = fn.legendre(3, 5)
+x = np.linspace(-1, 1, 200) 
+
+for i in range(1, 4):
+    plt.plot(x, fn.legendre(i, x), label ="Legendre: " + str(i)) 
+
+plt.legend(loc ="best")
+plt.xlabel("X")
+plt.ylabel("Pn")
+plt.show()
